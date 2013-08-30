e27dba00dcc3c7b6965558b8f768b1963d25614f
Nate Cleverly <nate@innitech.com>
2013-08-30 11:54:22 -0700

    Starting Commit

---
+++ XML to Json/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XML_to_Json.WebApiApplication" Language="C#" %>
---
+++ XML to Json/Models/url.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XML_to_Json.Models
+{
+    public class url
+    {
+        public string URL { get; set; }
+    }
+}
